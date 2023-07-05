2fdfb5fae68ce8ea4eae91243d08edfbc54759d3
Mario Vinti <mjvinti@gmail.com>
2023-07-05 16:13:54 -0400

    removed boilerplate code, setup global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import { Roboto_Slab } from "@next/font/google";
+
+import "@/styles/globals.css";
+
+const roboto = Roboto_Slab({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
