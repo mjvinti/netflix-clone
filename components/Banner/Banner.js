f0b0b965dc182376a6b8ff9e94875645db541192
Mario Vinti <mjvinti@gmail.com>
2023-07-07 15:57:58 -0400

    updated banner component w/ more styling

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -1,22 +1,28 @@
-import Image from "next/image";
+import styles from "./Banner.module.css";
 
 const Banner = ({ imgUrl, subTitle, title }) => {
-  const handleOnClick = () => console.log("playing");
+  const handleOnPlay = () => console.log("playing");
 
   return (
-    <div>
-      <h3>{title}</h3>
-      <h3>{subTitle}</h3>
-      <button onClick={handleOnClick}>Play</button>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
       <div
-        style={{
-          backgroundSize: "cover",
-          backgroundImage: `url(${imgUrl})`,
-          backgroundPosition: "50% 50%",
-          height: "100%",
-          position: "absolute",
-          width: "100%",
-        }}
+        className={styles.bannerImg}
+        style={{ backgroundImage: `url(${imgUrl})` }}
       />
     </div>
   );
