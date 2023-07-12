43815362cb4075559cfa9fb76f0bbcfa49a23965
Mario Vinti <mjvinti@gmail.com>
2023-07-12 12:16:47 -0400

    added card component sizing and styling

--- components/card/card.js
+++ components/card/card.js
@@ -1,10 +1,19 @@
 import Image from "next/image";
 
+import styles from "./card.module.css";
+
 const Card = ({ imgUrl, size }) => {
+  const classMap = {
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
+  };
+
   return (
-    <div>
-      Card
-      <Image alt="image" height={300} src={imgUrl} width={300} />
+    <div className={styles.container}>
+      <div className={classMap[size]}>
+        <Image alt="image" className={styles.cardImg} fill src={imgUrl} />
+      </div>
     </div>
   );
 };
