de1748aadbc230b3c6f39430afca5af520dd0948
Mario Vinti <mjvinti@gmail.com>
2023-07-12 13:11:56 -0400

    card component image error

--- components/card/card.js
+++ components/card/card.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 
 import styles from "./card.module.css";
 
-const Card = ({ imgUrl, size = "medium" }) => {
+const Card = ({
+  imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80",
+  size = "medium",
+}) => {
   const [imgSrc, setImgSrc] = useState(imgUrl);
 
   const classMap = {
@@ -12,7 +15,10 @@ const Card = ({ imgUrl, size = "medium" }) => {
     small: styles.smItem,
   };
 
-  const handleOnError = () => setImgSrc("/static/clifford.webp");
+  const handleOnError = () =>
+    setImgSrc(
+      "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
+    );
 
   return (
     <div className={styles.container}>
