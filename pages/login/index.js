a844b4552486abe87fa389ae946fe7c70963ce05
Mario Vinti <mjvinti@gmail.com>
2023-07-20 14:52:46 -0400

    began work on sign in header and sign in page structure

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,11 +1,48 @@
 import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from "@/styles/Login.module.css";
 
 const Login = () => {
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    console.log("onClick");
+  };
+
   return (
     <div>
       <Head>
         <title>Netflix SignIn</title>
       </Head>
+      <header>
+        <div className={styles.headerWrapper}>
+          <Link className={styles.logoLink} href="/">
+            <div className={styles.logoWrapper}>
+              <Image
+                alt="netflix logo"
+                height={34}
+                src="/static/netflix.svg"
+                width={128}
+              />
+            </div>
+          </Link>
+        </div>
+        <main className={styles.main}>
+          <div className={styles.mainWrapper}>
+            <h1 className={styles.signinHeader}>Sign In</h1>
+            <input
+              className={styles.emailInput}
+              placeholder="Email Address"
+              type="email"
+            />
+            <p className={styles.userMsg}></p>
+            <button className={styles.loginBtn} onClick={handleOnClick}>
+              Sign In
+            </button>
+          </div>
+        </main>
+      </header>
     </div>
   );
 };
