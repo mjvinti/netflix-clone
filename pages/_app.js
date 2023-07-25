046833333c5c4f99d0cfbf68cac9c2b97dadd2ca
Mario Vinti <mjvinti@gmail.com>
2023-07-25 16:19:02 -0400

    app route once logged in

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { Roboto_Slab } from "@next/font/google";
+
+import { magic } from "@/lib/magicClient";
 
 import "@/styles/globals.css";
 
 const roboto = Roboto_Slab({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    const isUserLoggedIn = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+      push(isLoggedIn ? "/" : "/login");
+    };
+
+    isUserLoggedIn();
+  }, []);
+
   return (
     <main className={roboto.className}>
       <Component {...pageProps} />
