5b826133b1217c3ac3017b0b8a78274844f43aff
Mario Vinti <mjvinti@gmail.com>
2023-07-26 17:08:59 -0400

    installed react modal

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,11 +1,26 @@
 import { useRouter } from "next/router";
+import Modal from "react-modal";
+
+Modal.setAppElement("#__next");
 
 const Video = () => {
   const {
+    back,
     query: { videoId },
   } = useRouter();
 
-  return <div>Video {videoId}</div>;
+  return (
+    <div>
+      Video {videoId}
+      <Modal
+        contentLabel="Watch the Video"
+        isOpen={true}
+        onRequestClose={() => back()}
+      >
+        <h1>hello there</h1>
+      </Modal>
+    </div>
+  );
 };
 
 export default Video;
