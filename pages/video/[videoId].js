450cd78d1e942bc27cf4dfff62d564b7e3b879f0
Mario Vinti <mjvinti@gmail.com>
2023-07-26 17:12:20 -0400

    added modal styling

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 
+import styles from "@/styles/Video.module.css";
+
 Modal.setAppElement("#__next");
 
 const Video = () => {
@@ -16,8 +18,9 @@ const Video = () => {
         contentLabel="Watch the Video"
         isOpen={true}
         onRequestClose={() => back()}
+        overlayClassName={styles.overlay}
       >
-        <h1>hello there</h1>
+        <h1>Modal Body</h1>
       </Modal>
     </div>
   );
