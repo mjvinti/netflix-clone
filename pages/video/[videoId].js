d0b0a88716c1d64e76beac5aefcfec82912edb07
Mario Vinti <mjvinti@gmail.com>
2023-07-27 11:39:05 -0400

    added more YT player styling

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -22,13 +22,14 @@ const Video = () => {
         overlayClassName={styles.overlay}
       >
         <iframe
+          className={styles.videoPlayer}
+          frameBorder="0"
+          height="360"
           id="ytplayer"
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           type="text/html"
           width="100%"
-          height="360"
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
         ></iframe>
-        <div>Modal Body</div>
       </Modal>
     </div>
   );
