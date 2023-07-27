dbcf6173c606b8c027d56d7ed872f4d97152c6ed
Mario Vinti <mjvinti@gmail.com>
2023-07-27 12:47:56 -0400

    added modal component structure

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import clsx from "classnames";
 import Modal from "react-modal";
 
 import styles from "@/styles/Video.module.css";
@@ -10,6 +11,16 @@ const Video = () => {
     back,
     query: { videoId },
   } = useRouter();
+
+  const video = {
+    title: "Hi cute dog",
+    publishTime: "1990-01-01",
+    description: "A big red dog that is super cute, can he get any bigger?",
+    channelTitle: "Paramount Pictures",
+    viewCount: 10000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
 
   return (
     <div className={styles.container}>
@@ -30,6 +41,25 @@ const Video = () => {
           type="text/html"
           width="100%"
         ></iframe>
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
